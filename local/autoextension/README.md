aaae6b075dd0387d1943291d93150b4e9d4d91e7
Marco Cigna <marco.cigna@gses.com.au>
2022-11-30 05:22:19 +0000

    removing unused plugin

--- local/autoextension/README.md
+++
@@ -1,2 +0,0 @@
-# sundayd-moodle-event-listener
-Auto + 2 weeks extension upon tutor marking their design task and providing feedback on moodle
--- local/autoextension/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2022101712;
-$plugin->requires = 2022041904.01;
-$plugin->component = 'local_autoextension';
